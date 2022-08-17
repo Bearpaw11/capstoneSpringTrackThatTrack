49fdbb6ee475d1ecd911984c8f944c9039fbd133
Chris Behrens <chrisbehrens@yahoo.com>
2022-08-16 21:14:17 -0700

    Updated formatting for signup.js

--- WebContent/resources/scripts/signup.js
+++ WebContent/resources/scripts/signup.js
@@ -1,36 +1,35 @@
-// Function to check if the password and confirm password are the same. If so the disabled is removed from the signup. 
- 
+// Function to check if the email is valid
 checkEmail = () => {
 	//first we set the email to a variable
 	let email = document.getElementById('email');
 	//set our regex to filter
-  	let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-   	//test the value of email
-    if (filter.test(email.value)) {
+	let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	//test the value of email
+	if (filter.test(email.value)) {
 		//if we pass the email check then we can call the check pass function
-	 	checkPass = () => {
+		checkPass = () => {
 			//first check to see if password and confirm are the smae
-    if (document.getElementById('password').value == document.getElementById('confirm').value) {
-		//check to see if passord is more that 5 characters 
-    	if(document.getElementById('password').value.length >=5) {
-			//if both are true then disabled is set to false on the Signup botton
-        	document.getElementById('submit').disabled = false;
-   		} else {
-		//if password is not at least 5 characters then alert and keep disabled true
-	 	document.getElementById('submit').disabled = true;
-		alert("passwords must be at least 5 charachters long")
-}
-    } else {
-		//if password and confirm are not equal alert and keep disabled true
-        document.getElementById('submit').disabled = true;
-		alert("passwords do not match")
-    }
-}
-} else {
-	alert("Please enter a vaild email")
-}
+			if (document.getElementById('password').value == document.getElementById('confirm').value) {
+				//check to see if passord is more that 5 characters 
+				if (document.getElementById('password').value.length >= 5) {
+					//if both are true then disabled is set to false on the Signup botton
+					document.getElementById('submit').disabled = false;
+				} else {
+					//if password is not at least 5 characters then alert and keep disabled true
+					document.getElementById('submit').disabled = true;
+					alert("passwords must be at least 5 charachters long")
+				}
+			} else {
+				//if password and confirm are not equal alert and keep disabled true
+				document.getElementById('submit').disabled = true;
+				alert("passwords do not match")
+			}
+		}
+	} else {
+		alert("Please enter a vaild email")
+	}
 }
 
 
- 
+
 
